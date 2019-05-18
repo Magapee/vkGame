2e27349f278750aa7408860285a2e91e8f31be86
tellursium <egor_starkov@mail.ru>
2019-05-18 14:07:19 +0300

    start

--- vk_mmo_game/vk_mmo_game/litedb.py
+++
@@ -1,3 +0,0 @@
-
-class LiteDatabase(object):
-    raise NotImplemented
--- vk_mmo_game/vk_mmo_game/vkgame.py
+++ vk_mmo_game/vk_mmo_game/vkgame.py
@@ -1,30 +1,50 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-#from litedb import LiteDatabase
-#from player import Player
-from vk_token import token
+import random
 
+from litedatabase import LiteDatabase
+from player import Player
+import keyboard
+import logger
+import constants
 
-class Game(object):
+class Game():
     def __init__(self):
-        print('Starting...') #заменить на логгер
-        self.is_running = False
-
-        self.vk_session = vk_api.VkApi(token = token)
-        self.vk_session._auth_token()
-        self.vk = self.vk_session.get_api()
-        self.longpoll = VkLongPoll(self.vk_session)
-
+        logger.logger()
+        vk_session = vk_api.VkApi(token = constants.token)
+        try:
+            vk_session.auth(token_only=True)
+        except vk_api.AuthError as error_msg:
+            logger.log(error_msg)
+        self.vk = vk_session.get_api()
+        self.longpoll = VkLongPoll(vk_session)
         self.is_running = True
-
-        print('Started!')
+        #self.counter = 0
+        self.player = Player()
 
     def process(self):
         while self.is_running:
-            event = self.lonlongpoll.check()
-            if event:
-                _proc_event(event)
+            #print(len(self.longpoll.check()), self.counter)
+            #event = self.longpoll.check()[self.counter]
+            #if event:
+            for event in self.longpoll.listen():
+                self._proc_event(event)
+                if self.is_running == False:#Временный функционал, для тестов
+                    break
            
     def _proc_event(self, event):
         if not event.from_me:
             if event.type == VkEventType.MESSAGE_NEW and event.text:
+                #self.counter = self.counter + 1
+                random.seed()
+                database = LiteDatabase()
+                #---------------------------------------------------------------stop
+                self.is_running = self.player.stop(self.vk, event)
+                #---------------------------------------------------------------stop
+                self.player.reg(self.vk, event, database)#Регистрация
+                if self.player.is_registered(event.user_id, database):#Проверка для ивентов
+                    self.player.quest(self.vk, event, database)
+                    self.player.hero(self.vk, event, database)
+                    self.player.get_battle_link(self.vk, event, database)
+                    self.player.check_battle_link(self.vk, event, database)
+                    self.player.get_battle_stats(self.vk, event, database)
