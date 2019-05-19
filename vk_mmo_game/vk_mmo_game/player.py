f66daf94222d55be0f01f40a2cad97d5bdc42dbc
tellursium <egor_starkov@mail.ru>
2019-05-19 21:18:47 +0300

    исправил мэджик намберс-хуямберс

--- vk_mmo_game/vk_mmo_game/player.py
+++ vk_mmo_game/vk_mmo_game/player.py
@@ -4,6 +4,9 @@ import keyboard
 import getheromessage
 import const
 import str_const
+from str_const import db_names
+from str_const import event_calls
+from str_const import messages
 from litedb import LiteDB
 
 import vk_api
@@ -17,10 +20,10 @@ class PlayerManager():
         self.duel_links = { }
 
     def quest(self, vk, event, database):#Квест, надо допилить
-        database.set("""exp""", """exp + 1""", event.user_id)
-        logger.log("+1 exp", event.user_id)
-        database.set("""lvl""", database.checklvl(database.select("""exp""", """id""", event.user_id)[0][0], event.user_id), event.user_id)
-        vk.messages.send(user_id=event.user_id, message='Ok', random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
+        database.set(db_names.exp, db_names.exp + str_const.plus, event.user_id)
+        logger.log("quest", event.user_id)
+        database.set(db_names.lvl, database.checklvl(database.select(db_names.exp, db_names.id, event.user_id)[0][0], event.user_id), event.user_id)
+        vk.messages.send(user_id=event.user_id, message=messages.ok, random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
 
     def hero(self, vk, event, database):#Вызов профиля
         logger.log("hero", event.user_id)
@@ -28,32 +31,32 @@ class PlayerManager():
         return True
 
     def stop(self, vk, event):#Остановка бота, тестовый функционал
-        if event.text == 'Stop':
+        if event.text == db_names.stop:
             if event.user_id in const.admins:
-                vk.messages.send(user_id=event.user_id, message='Ok', random_id = random.randrange(1, 10000, 1))
+                vk.messages.send(user_id=event.user_id, message=messages.ok, random_id = random.randrange(1, 10000, 1))
                 logger.log("Stop", event.user_id)
                 return False
             else:
-                vk.messages.send(user_id=event.user_id, message='Функционал для админов', random_id = random.randrange(1, 10000, 1))
+                vk.messages.send(user_id=event.user_id, message=messages.admin_func, random_id = random.randrange(1, 10000, 1))
                 return True
         else:
             return True
 
     def reg(self, vk, event, database):#Регистрация пользователя
-        cur = database.select("""countryid""", """id""", event.user_id)
+        cur = database.select(db_names.countryid, db_names.id, event.user_id)
         if cur == []:
-            vk.messages.send(user_id=event.user_id, message='Выберите фракцию', random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(1, event.user_id))
+            vk.messages.send(user_id=event.user_id, message=messages.select_frac, random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(1, event.user_id))
             database.insert_new(event.user_id)
         elif cur == [(0,)]:
             if event.text in str_const.fracs1:
-                database.set("""countryid""", str_const.fracs1[event.text], event.user_id)
-                vk.messages.send(user_id=event.user_id, message='Вами выбрана фракция: ' + event.text, random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
+                database.set(db_names.countryid, str_const.fracs1[event.text], event.user_id)
+                vk.messages.send(user_id=event.user_id, message=messages.you_faction + event.text, random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
             else:
-                vk.messages.send(user_id=event.user_id, message='Выберите фракцию', random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
+                vk.messages.send(user_id=event.user_id, message=messages.select_frac, random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
             logger.log("registration", event.user_id)
 
     def is_registered(self, user_id, database):#Проверка, зарегистрирован ли пользователь
-        cur = database.select("""countryid""", """id""", user_id)
+        cur = database.select(db_names.countryid, db_names.id, user_id)
         if cur != []:
             if cur[0][0] != 0:
                 return(True)
@@ -63,41 +66,39 @@ class PlayerManager():
 
     def get_battle_link(self, vk, event, database): #генерация ссылки для дуэли
         logger.log("battle", event.user_id)
-        link = """duel_""" + str(self.count)
+        link = "duel_" + str(self.count)
         self.count = self.count + 1
         self.duel_links[link] = event.user_id
         vk.messages.send(user_id=event.user_id, message=link, random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
     
     def check_battle_link(self, vk, event, database): #дуэль по ссылке
-        id = self.duel_links.get(event.text)
+        id = self.duel_links.pop(event.text)
         if id != None:
             if id != event.user_id:
-                database.set("""battlelink""", """NULL""", id)
-                database.set("""battlelink""", """NULL""", event.user_id)
                 cube = random.randrange(0, 2, 1)
-                cube1 = {-1:"Победа", 0:"Поражение", 1:"Победа"}
+                cube1 = {-1:messages.win, 0:messages.lose, 1:messages.win}
                 if cube == 1:
-                    database.set("""winscounter""", """winscounter + 1""", event.user_id)
+                    database.set(db_names.winscounter, db_names.winscounter + str_const.plus, event.user_id)
                 if cube == 0:
-                    database.set("""winscounter""", """winscounter + 1""", id)
+                    database.set(db_names.winscounter, db_names.winscounter + str_const.plus, id)
                 vk.messages.send(user_id=event.user_id, message=cube1[cube], random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
                 vk.messages.send(user_id=id, message=cube1[cube - 1], random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, id))
             else:
-                vk.messages.send(user_id=event.user_id, message="Нельзя драться с собой", random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
+                vk.messages.send(user_id=event.user_id, message=messages.fight_yourself, random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
 
     def get_battle_stats(self, vk, event, database):
-        vk.messages.send(user_id=event.user_id, message="Побед:" + str(database.select("""winscounter""", """id""", event.user_id)[0][0]), random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
+        vk.messages.send(user_id=event.user_id, message=messages.wins + str(database.select(db_names.winscounter, db_names.id, event.user_id)[0][0]), random_id = random.randrange(1, 10000, 1), keyboard = keyboard.getKey(2, event.user_id))
         return True
 
     def event_handling(self, vk, event, database):
         if self.is_registered(event.user_id, database):
-            if event.text == "exp +1":
+            if event.text == event_calls.quest:
                 self.quest(vk, event, database)
-            elif event.text == "hero":
+            elif event.text == event_calls.hero:
                 self.hero(vk, event, database)
-            elif event.text == "Статистика":
+            elif event.text == event_calls.stat:
                 self.get_battle_stats(vk, event, database)
-            elif  event.text == "Дуэль":
+            elif  event.text == event_calls.duel:
                 self.get_battle_link(vk, event, database)
             else:
                 self.check_battle_link(vk, event, database)
--- vk_mmo_game/vk_mmo_game/str_const.py
+++ vk_mmo_game/vk_mmo_game/str_const.py
@@ -17,6 +17,33 @@ fracs_quantity = 3
 fracs0 = {1:'Сумрачный замок', 2:'Мятный замок', 3:'Пидорский замок'}
 fracs1 = { }
 
+class db_names():
+    id = "id"
+    exp = "exp"
+    lvl = "lvl"
+    countryid = "countryid"
+    quest = "exp +1"
+    winscounter = "winscounter"
+    stop = "Stop"
+
+class event_calls():
+    hero = "hero"
+    quest = "exp +1"
+    stat = "Статистика"
+    duel = "Дуэль"
+
+class messages():
+    ok = 'Ok'
+    admin_func = 'Функционал для админов'
+    select_frac = 'Выберите фракцию'
+    you_faction = "Вами выбрана фракция: "
+    win = "Победа"
+    lose = "Поражение"
+    fight_yourself = "Нельзя драться с собой"
+    wins = "Побед:"
+
+plus = " + 1"
+
 def set_fracs_list():
     for i in range(1, fracs_quantity):
         fracs1[i] = fracs0[i]
